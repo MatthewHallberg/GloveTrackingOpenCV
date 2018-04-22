62ff943fc7cbbe0d567d7ccc685400090e7a927c
matthew hallberg <matthewhallberg@gmail.com>
2018-04-22 03:31:44 -0400

    receiving messages from python.

---
+++ Assets/SendMessages.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+/// <summary>
+/// This sends and recieves UDP packets to and from python
+/// </summary>
+
+namespace OpenCVForUnitySample {
+	public class SendMessages : MonoBehaviour {
+
+		public MultiObjectTrackingBasedOnColorSample handTracking;
+
+		private const int PORT_NUM = 1996;
+		private const float UPDATE_TIME = .5f;
+		private string serverIP = "10.0.0.109";
+		private IPAddress serverAddr;
+		private IPEndPoint endPoint;
+		private Socket sock;
+		private byte[] send_buffer;
+
+		void Start(){
+			if (PlayerPrefs.HasKey("IP")){
+				serverIP = PlayerPrefs.GetString("IP");
+			}
+		}
+
+		void OnDisable(){
+			CloseSocket ();
+		}
+
+		/// <summary>
+		/// This gets called when the IP address is changed in the input field
+		/// </summary>
+		public void SetIP(string IP){
+			serverIP = IP;
+			PlayerPrefs.SetString ("IP", serverIP);
+			Debug.Log ("New Server IP: " + IP);
+			InitSocket ();
+		}
+
+		void InitSocket(){
+			//close socket in case its already open
+			CloseSocket ();
+			//init socket
+			sock = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			serverAddr = IPAddress.Parse (serverIP);
+			endPoint = new IPEndPoint (serverAddr, PORT_NUM);
+			SendPositionMessages();
+		}
+
+		void CloseSocket(){
+			if (sock != null) {
+				StopAllCoroutines ();
+				sock.Disconnect (true);
+			}
+		}
+
+		void SendPositionMessages(){
+			Debug.Log ("Starting Messages...");
+			InitSocket ();
+			StartCoroutine (SendPosition ());
+		}
+			
+		IEnumerator SendPosition(){
+			while (true) {
+				string text = handTracking.GetHandPositions ();
+				send_buffer = Encoding.ASCII.GetBytes(text);
+				sock.SendTo(send_buffer,endPoint);
+				print (text);
+				yield return new WaitForSeconds (UPDATE_TIME);
+			}
+		}
+	}
+}
+
---
+++ Assets/UDPReceive.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceive : MonoBehaviour {
+
+	private const int PORT = 1991;
+	public Text holdingText;
+	public Text blockingText;
+
+	private string lastPacketText;
+	private bool gotNewPacket = false;
+	private Thread receiveThread;
+	private UdpClient client;
+
+	private static void Main() {
+		UDPReceive receiveObj=new UDPReceive();
+		receiveObj.init();
+	}
+	// start from unity3d
+	public void Start() {
+		init();
+	}
+
+	void Update(){
+		//listen for changes on message thread
+		if (gotNewPacket) {
+			gotNewPacket = false;
+			GotNewMessage ();
+		}
+	}
+
+	private void init(){
+		receiveThread = new Thread(new ThreadStart(ReceiveData));
+		receiveThread.IsBackground = true;
+		receiveThread.Start();
+
+	}
+
+	// receive thread
+	private  void ReceiveData() {
+		client = new UdpClient(PORT);
+		while (true && client != null) {
+			try {
+				IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+				byte[] data = client.Receive(ref anyIP);
+				string text = Encoding.UTF8.GetString(data);
+				lastPacketText = text;
+				gotNewPacket = true;
+			} catch (Exception err){
+				print(err);
+			}
+		}
+	}
+
+	void GotNewMessage(){
+		//new messages come in here!
+
+
+
+		holdingText.text = lastPacketText;
+	}
+
+	void OnDisable() { 
+		if (receiveThread != null) {
+			receiveThread.Abort ();
+		}
+		client.Close(); 
+	}
+}
